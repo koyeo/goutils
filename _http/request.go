d054ea83708a86f66a3e19f351a8a43df6d31ef9
koyeo <koyeo@qq.com>
2021-05-11 18:18:13 +0800

    init

--- _http/request.go
+++ _http/request.go
@@ -39,85 +39,83 @@ func (p *Request) SetContentType(contentType string) *Request {
 }
 
 func (p *Request) request(method, url string, data interface{}) (response *Response, err error) {
-	
+
 	contentType := p.Headers().Get(CONTENT_TYPE)
 	if contentType == "" {
 		p.SetContentType(APPLICATION_JSON)
 	}
-	
+
 	body, err := p.formatBody(contentType, data)
 	if err != nil {
 		err = fmt.Errorf("format request data error: %s", err)
 		return
 	}
-	
+
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		err = fmt.Errorf("http.NewRequest error: %s", err)
 		return
 	}
-	
+
 	for key, val := range p.Headers().Headers() {
 		req.Header.Add(key, val)
 	}
-	
+
 	resp, err := client.Do(req)
 	if err != nil {
 		err = fmt.Errorf("client.Do error: %s", err)
 		return
 	}
-	
+
 	if resp.StatusCode != 200 {
 		err = fmt.Errorf("response code error: %d", resp.StatusCode)
 		return
 	}
-	
+
 	response = NewResponse()
 	response.bytes, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		err = fmt.Errorf("reade body error: %s", err)
 		return
 	}
-	
+
 	return
 }
 
 func (p *Request) formatBody(contentType string, data interface{}) (body io.Reader, err error) {
 	switch contentType {
-	case APPLICATION_X_WWW_FORM_URLENCODED:
-	case APPLICATION_JSON:
+	case APPLICATION_X_WWW_FORM_URLENCODED, APPLICATION_JSON:
 		return p.formatJson(data)
 	case TEXT_XML:
 	case MULTIPART_FORM_DATA:
-	
 	}
 	return
 }
 
 func (p *Request) formatJson(data interface{}) (body io.Reader, err error) {
-	
+
 	t := reflect.TypeOf(data)
-	switch t.Kind().String() {
-	case "slice":
-		if t.Elem().String() == "uint8" {
+	switch t.Kind() {
+	case reflect.Slice:
+		if t.Elem().Kind() == reflect.Uint8 {
 			body = bytes.NewReader(data.([]byte))
 			return
 		}
-	case "string":
+	case reflect.String:
 		body = bytes.NewReader([]byte(data.(string)))
 		return
 	}
-	
+
 	// TODO 判断其它类型
-	
+
 	var bs []byte
 	bs, err = json.Marshal(data)
 	if err != nil {
 		return
 	}
 	body = bytes.NewReader(bs)
-	
+
 	return
 }
 
